2b3768a66a809fa078dfd8f8fb5dfd12f5c3c5ad
Audia Winniar Savitri <audiaws@Gians-MacBook-Air.local>
2024-03-03 15:33:37 -0600

    update: register api

--- miniprojectDibimbing_react_audia/src/pages/Register.jsx
+++ miniprojectDibimbing_react_audia/src/pages/Register.jsx
@@ -1,10 +1,14 @@
 import  { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './register.css'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function RegistrationForm() {
+const RegistrationForm = () => {
+    const [notif, setNotif] = useState("");
+    const [notifError, setNotifError] = useState("");
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
-        name: '',
         email: '',
         password: '',
       });
@@ -19,6 +23,26 @@ function RegistrationForm() {
       const handleSubmit = (e) => {
         e.preventDefault();
         console.log(formData);
+
+        const payload = {
+          email: formData.email,
+          password: formData.password
+        };
+        axios
+          .post('https://reqres.in/api/register', payload)
+          .then((response) => {
+            setNotif ("Register Success");
+            const token = response.data.token;
+            localStorage.setItem("token", token);
+            console.log(response);
+            setTimeout(() => {
+              navigate("/");
+            }, 3000);
+          })
+          .catch((error) => {
+            console.log(error.response);
+            setNotifError(error.response.data.error);
+          });
       };
     
   return (
@@ -34,10 +58,7 @@ function RegistrationForm() {
         </div>
         <div className={`rightColumn col-md-6 `}>
           <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">Name</label>
-              <input type="text" className="form-control" id="name" name="name" onChange={handleChange} />
-            </div>
+            <h2>Register</h2>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Email ID</label>
               <input type="email" className="form-control" id="email" name="email" onChange={handleChange} />
@@ -45,12 +66,14 @@ function RegistrationForm() {
             <div className="mb-3">
               <label htmlFor="password" className="form-label">Password</label>
               <input type="password" className="form-control" id="password" name="password" onChange={handleChange} />
+              {!!notifError.length && <h3>{notifError}</h3>}
             </div>
             <div className="mb-3 form-check">
               <input type="checkbox" className="form-check-input" id="terms" />
               <label className="form-check-label" htmlFor="terms">I Accept terms and conditions & privacy policy</label>
             </div>
             <button type="submit" className="btn btn-primary">Register</button>
+            {!!notif.length && <h4>{notif}</h4>}
           </form>
         </div>
       </div>
